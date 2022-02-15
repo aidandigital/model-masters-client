e4626bd180f27ac69a7f0bf0da5df97120681876
Aidan <84754533+aidan10371@users.noreply.github.com>
2022-02-14 19:26:41 -0500

    cookie instructions

--- src/components/errorPages/NoCrossCookies.jsx
+++ src/components/errorPages/NoCrossCookies.jsx
@@ -2,18 +2,22 @@ import React from "react";
 import Button from "../parts/Button";
 import Title from "../parts/Title";
 import { RefreshIcon } from "@heroicons/react/outline";
+import { QuestionMarkCircleIcon } from "@heroicons/react/outline";
 
 const NoCrossCookies = () => (
   <div className="text-center">
     <Title>3rd Party Cookies are Disabled</Title>
     <p className="m-3">In your browser settings, turn on 3rd party cookies (also known as cross site tracking).</p>
-    <p className="bg-secondarydark cursor-pointer p-3" onClick={() => {alert(
+    <Button type="important" onClick={() => {alert(
         `
-        iPhone: Go to Settings > Safari > then uncheck "prevent cross site tracking"
-        Laptop (Safari): Go to Prefrences > Privacy > then uncheck "prevent cross site tracking"
-        Laptop (Chrome): Go to Settings > Security & Privacy > Cookies & other site data > Allow all cookies *OR* block cookies in cognito
+        iPhone: Go to Settings > Safari > uncheck "prevent cross site tracking"
+
+        Laptop (Safari): Go to Prefrences > Privacy > uncheck "prevent cross site tracking"
+
+        Laptop (Chrome): Go to Settings > Security & Privacy > Cookies & other site data > Select "Allow all cookies" OR "block 3rd party cookies in cognito"
         `
-    )}}>How do I do this?</p>
+    )}}><QuestionMarkCircleIcon className="h-5 relative -top-0.5 inline-block" /> How do I fix this?</Button>
+    <div className="h-3"></div>
     <a href="/"><Button icon={RefreshIcon}>Refresh</Button></a>
   </div>
 );
