93fd5649b6db07485457f29c2252846cfb47a752
Aidan <84754533+aidan10371@users.noreply.github.com>
2022-02-14 19:13:44 -0500

    cookie instructions

--- src/components/errorPages/NoCrossCookies.jsx
+++ src/components/errorPages/NoCrossCookies.jsx
@@ -6,7 +6,14 @@ import { RefreshIcon } from "@heroicons/react/outline";
 const NoCrossCookies = () => (
   <div className="text-center">
     <Title>3rd Party Cookies are Disabled</Title>
-    <p className="m-3">In your browser settings, turn on 3rd party cookies (also known also cross site tracking).</p>
+    <p className="m-3">In your browser settings, turn on 3rd party cookies (also known as cross site tracking).</p>
+    <p onClick={() => {alert(
+        `
+        iPhone: Go to Settings > Safari > then uncheck "prevent cross site tracking"
+        Laptop (Safari): Go to Prefrences > Privacy > then uncheck "prevent cross site tracking"
+        Laptop (Chrome): Go to Settings > Security & Privacy > Cookies & other site data > Allow all cookies *OR* block cookies in cognito
+        `
+    )}}>How do I do this?</p>
     <a href="/"><Button icon={RefreshIcon}>Refresh</Button></a>
   </div>
 );
