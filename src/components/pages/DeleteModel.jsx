645fb663966958df09587e77dd2d39aad48be156
Aidan <84754533+aidan10371@users.noreply.github.com>
2022-02-19 13:24:00 -0500

    del model chngs

--- src/components/pages/DeleteModel.jsx
+++ src/components/pages/DeleteModel.jsx
@@ -66,20 +66,6 @@ render() {
         <Section fullHeight={true}>
             <Title>Delete a Model</Title>
             <form className="w-full sm:w-4/5 md:w-3/5 m-auto">
-                <Label>Model ID</Label>
-                <TextInput disabled="disabled" name="name" type="text" value={this.state.model_id} error={this.state.message.model_id} />
-                {/*
-                <Label>Reason for deletion</Label>
-                <div className="rounded-md border my-3" onChange={this.setInput}>
-                    <div><RadioButton name="reason" value="privacy" description="I just want this model removed" /></div>
-                    <div><RadioButton name="reason" value="accident" description="I accidentally published this" /></div>
-                    <div><RadioButton name="reason" value="sensitive" description="I submitted sensitive info" /></div>
-                    <div><RadioButton name="reason" value="error" description="An error occured making this" /></div>
-                    <div><RadioButton name="reason" value="remake" description="I'm going to re-publish this" /></div>
-                    <div><RadioButton name="reason" value="other" description="Other" /></div>
-                </div>
-                {this.state.message.type}
-                */}
                 <Label>Password</Label>
                 <TextInput name="password" type="password" onChange={this.setInput} value={this.state.password} error={this.state.message.password} placeholder="Enter your password to verify this" />
                 <Spacer height="3" />
