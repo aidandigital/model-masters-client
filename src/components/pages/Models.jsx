2526edac86bab785d0d56ad7e4c4088dd998ed34
Aidan <84754533+aidan10371@users.noreply.github.com>
2022-02-19 19:50:19 -0500

    select models order

--- src/components/pages/Models.jsx
+++ src/components/pages/Models.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Row from "../parts/Row";
 import Column from "../parts/Column";
 import Section from "../parts/Section";
@@ -8,27 +9,48 @@ import UserLink from "../parts/UserLink";
 import Header from "../parts/Header";
 import Footer from "../parts/Footer";
 
-const Models = (props) => (
-  <>
-    <Header />
-    <Section fullHeight={true}>
-      <Title color="black">All Models</Title>
-      {props.data.map((model, i) => (
-        <Column key={i}>
+const Models = (props) => {
+  const [models, setModels] = useState(props.data); // Making copies of state is generally not a good idea incase it updates in the parent, but we need it here
+
+  function reverseModelOrder() {
+    let modelsCopy = [...models];
+    modelsCopy.reverse()
+    setModels(modelsCopy)
+  }
+
+  return (
+    <>
+      <Header />
+      <Section fullHeight={true}>
+        <Title color="black">All Models</Title>
+        <div className="text-center">
+          <p className="px-2 inline-block">Show</p>
+          <select className="pr-5 cursor-pointer focus:outline-none" onChange={reverseModelOrder} defaultValue="Newest" /* Our data comes in with newest first */>
+            <option value="Newest">Newest</option>
+            <option value="Oldest">Oldest</option>
+          </select>
+          <p className="px-2 inline-block">Models first</p>
+        </div>
+        {models.map((model, i) => (
+          <Column key={i}>
             <Row>
               <div className="text-center mb-1">
                 <Link to={"/model/" + model._id}>
                   <Image src={model.thumbnail} isThumbnail={true} />
-                  <p className="mt-5 font-bold mb-3 text-xl font-serif hover:text-primarydark duration-200">{model.name}</p>
+                  <p className="mt-5 font-bold mb-3 text-xl font-serif hover:text-primarydark duration-200">
+                    {model.name}
+                  </p>
                 </Link>
-                  {model.type} by<UserLink _id={model.user._id} name={model.user.name} />
+                {model.type} by
+                <UserLink _id={model.user._id} name={model.user.name} />
               </div>
             </Row>
-        </Column>
-      ))}
-    </Section>
-    <Footer />
-  </>
-);
+          </Column>
+        ))}
+      </Section>
+      <Footer />
+    </>
+  );
+};
 
 export default Models;
