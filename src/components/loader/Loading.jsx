ba620322213a5a3242e76c995c261e5a85df90e8
aidan10371 <84754533+aidan10371@users.noreply.github.com>
2022-08-12 16:33:02 -0400

    Update Loading.jsx

--- src/components/loader/Loading.jsx
+++ src/components/loader/Loading.jsx
@@ -3,7 +3,7 @@ import "./loader.css";
 const Loading = (props) => (
     <>
         <div className="cn"><div {...props} className="ajax-loader"></div></div>
-        <span className="px-16 w-full absolute bottom-40 text-center">
+        <span className="px-16 w-full absolute bottom-10 md:bottom-40 text-center">
             <div>This app is brought to you on a free server.</div>
             <div>Please be patient, this may take a moment at first.</div>
         </span>
